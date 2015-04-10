66d96f0eb293c8da7ed299f9418dc7a892f46319
Francis Besset <francis.besset@gmail.com>
2015-04-10 07:57:11 +0200

    Fixed html homepage

--- src/front_controller.php
+++ src/front_controller.php
@@ -13,10 +13,10 @@ $front->get('/', function() use ($app) {
     }
 
     return new Response(
-        '<?xml version="1.0" encoding="UTF-8" ?>'.
         '<!DOCTYPE html>'.
         '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">'.
             '<head>'.
+                '<meta charset="utf-8">'.
                 '<title>Synology Packages</title>'.
             '</head>'.
             '<body>'.
